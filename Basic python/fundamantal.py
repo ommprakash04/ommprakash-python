142b88cf493ca0501754d1759a8823a908f71830
ommprakash04 <ommprakashsahoo790@gmail.com>
2025-08-22 16:32:50 +0530

    data

---
+++ Basic python/fundamantal.py
@@ -0,0 +1,11 @@
+#DATA TYPE
+# Data type are the catagory that specify which type of value a variable store.
+# 1. Number type:-
+# Ther are three type of number data type which is store numeric value.
+# A. Intiger:-
+# it store whole +ve / -ve number.
+# Example
+a = 10
+b = -20
+print(a)
+print(b)
